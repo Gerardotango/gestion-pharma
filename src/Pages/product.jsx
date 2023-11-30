c1ee9528ee2d0dd64329a7b97a8d6067915b8dfb
Gerardotango <tangogerard890@gmail.com>
2023-11-30 12:36:24 +0100

    add elements of pages

---
+++ src/Pages/product.jsx
@@ -0,0 +1,13 @@
+
+import { Outlet } from 'react-router-dom'
+
+function Product() {
+    return (
+        <>
+        <h1>product</h1>
+        <Outlet/>
+
+        </>
+    )
+}
+export default Product;
---
+++ src/Pages/singleproduct.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function SingleProduct() {
+    return (
+        <h1>singleproduct</h1>
+    )
+}
+export default SingleProduct;
