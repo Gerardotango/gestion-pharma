0440721e53d123b98f2ba349db7ec5d4b51d6172
Gerardotango <tangogerard890@gmail.com>
2023-11-30 12:41:42 +0100

    add navbar in components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <nav>
+      <ul>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+        <li><NavLink to='/login'>Login</NavLink></li>
+        <li><NavLink to='/product'>Product</NavLink></li>
+      </ul>
+    </nav>
+    )
+}
+
+export default NavBar ;
